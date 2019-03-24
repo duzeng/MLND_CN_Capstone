ab8efa2449fd0fcaa41628a6186d09ae85668007
duzeng <duzengrass@163.com>
2019-03-24 14:33:03 +0800

    chore: 基本模块
    Signed-off-by: duzeng <duzengrass@163.com>

---
+++ import_file.py
@@ -0,0 +1,19 @@
+# Standard Scientific Import
+from IPython.display import display, HTML, Javascript,set_matplotlib_formats
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import plot as plot
+import sklearn
+from joblib import Parallel, delayed
+import seaborn as sns
+sns.set()
+sns.set_style('whitegrid')
+set_matplotlib_formats('png', 'pdf')
+# set_matplotlib_formats('png','svg','pdf')
+
+# Module Imports
+
+# Custom Imports
